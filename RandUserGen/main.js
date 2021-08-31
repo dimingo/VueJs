29a7fc01141dba2687a32abf3b04fd3b0a472d99
mitchel Ketcha <micalvin98@gmail.com>
2021-08-31 23:48:20 +0300

    second Commit

--- RandUserGen/main.js
+++ RandUserGen/main.js
@@ -1,5 +1,27 @@
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
+Vue.component('message', {
+
+    props: ['title', 'body'],
+
+    data(){
+        
+        return {
+            isVisible: true
+        }
+    },
+    
+    template: `
+    <article class="message" v-show="isVisible">
+    <div class="message-header" >
+       {{ title }}
+       
+       <button type="button" @click="isVisible=false">X</button>
+    </div>
+    <div class="message-body">
+        {{body}}
+    </div>
+</article>
+`,
+
 });
 
 new Vue({
