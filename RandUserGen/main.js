eb0e6ed5bea728c9b3fc24082070e5c824a54710
dimingo <micalvin98@gmail.com>
2021-09-01 17:18:29 +0300

    third Commit

--- RandUserGen/main.js
+++ RandUserGen/main.js
@@ -1,30 +1,44 @@
-Vue.component('message', {
+Vue.component('modal', {
 
-    props: ['title', 'body'],
-
-    data(){
-        
-        return {
-            isVisible: true
-        }
-    },
-    
     template: `
-    <article class="message" v-show="isVisible">
-    <div class="message-header" >
-       {{ title }}
-       
-       <button type="button" @click="isVisible=false">X</button>
-    </div>
-    <div class="message-body">
-        {{body}}
-    </div>
-</article>
-`,
+    <div class='modal is-active'>
+    <div class="modal-background"></div>
 
-});
+    <div class='modal-card'>
+        <header class="modal-card-head">
+           <p class='modal-card-title'>Modal titles</p>
+           <button class='delete'></button>
+    </header>
+
+    <section class="modal-card-body">
+
+        Temporary Content for Now
+
+    </section>
+
+    <footer class='modal-card-foot'>
+        <a class="button is-primary" >Save Changes</a>
+        <a class="button" >Cancel</a>
+
+    </footer>
+    
+</div>
+</div>
+    `
+})
 
 new Vue({
-    el:'#root'
+    el: '#root',
+
+
+    data: {
+        couponApplied: false
+    },
+
+
+    created() {
+        Event.listen('applied', () => alert('Handling it'))
+    }
+
 
 });
